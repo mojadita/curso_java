7716168928e7f86af1323efc920996f2ca9391da
anb00 <anb00>
2019-03-08 00:30:21 +0100

    Added some lines. But the Constructor doesnt work

--- HelloWorld/src/chaterz/java/course/Bombilla.java
+++ HelloWorld/src/chaterz/java/course/Bombilla.java
@@ -1,7 +1,15 @@
 package chaterz.java.course;
 
 public class Bombilla {
-	
+
+	public void bombilla(){}
+
+	public void setEncendido(boolean encendido) {
+		this.encendido = encendido;
+	}
+
+	/* boolean no tiene get, al generar getter an setter solo genera setter ? */
+
 	boolean encendido = false;
 	
 	public void encender() {
@@ -12,6 +20,42 @@ public class Bombilla {
 		System.out.println(this + ": me han apagado");
 		encendido = false;
 	}
-	public boolean isEncendido() { return encendido; }
+	public boolean isEncendido() { return encendido; } /* <-- esto no lo piyo */
+
+	public boolean isAfluorescente() { return tipo; }
+
+
+
+	/* Solo se hacer construcctores a partir de Strings o instancias */
+	public String getPotencia() {
+		return potencia;
+	}
+
+
+
+	public void setPotencia(String potencia) {
+		this.potencia = potencia;
+	}
+
+	public int getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+
+	/**
+	 * My Mombilla
+	 */
+	/* public String luz;
+	public String vatios;
+	public String forma;
+	public String tipo;
+	public String precio;
+	*/
+
+	public String potencia;
+	public int precio;
 
 }
--- HelloWorld/src/chaterz/java/course/HelloWorld.java
+++ HelloWorld/src/chaterz/java/course/HelloWorld.java
@@ -4,6 +4,17 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Buenas!!");
 		Bombilla b1 = new Bombilla();
+		if (b1 = new Bombilla() ) {
+			System.out.println("Se ha Creado la Bombilla b1");
+		}else {}
+
+		if (b2 = new Bombilla() ){
+			System.out.println("Se ha Creado la Bombilla b2");
+		}
 		Bombilla b2 = new Bombilla();
+		Bombilla b3 = new Bombilla();
+
+		System.out.println(b2);
+		System.out.println(b1);
 	}
 }

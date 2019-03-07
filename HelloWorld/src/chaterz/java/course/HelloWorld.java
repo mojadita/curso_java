3d7b0ce6dcefa0dbfe558d8362ca171de6e9b570
Luis Colorado <luiscoloradourcola@gmail.com>
2019-03-07 18:56:12 +0200

    Print bulbs status in main

--- HelloWorld/src/chaterz/java/course/HelloWorld.java
+++ HelloWorld/src/chaterz/java/course/HelloWorld.java
@@ -5,5 +5,7 @@ public class HelloWorld {
 		System.out.println("Buenas!!");
 		Bombilla b1 = new Bombilla();
 		Bombilla b2 = new Bombilla();
+		System.out.println(b1 + ".isEncendido() == " + b1.isEncendido());
+		System.out.println(b2 + ".isEncendido() == " + b2.isEncendido());
 	}
 }

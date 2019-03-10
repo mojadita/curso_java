a250704d4b3e8b77e827e1f041b392c2f5b923c6
Luis Colorado <luiscoloradourcola@gmail.com>
2019-03-10 15:25:34 +0200

    Formatted code
    Signed-off-by: Luis Colorado <luiscoloradourcola@gmail.com>

--- HelloWorld/src/chaterz/java/course/Bombilla.java
+++ HelloWorld/src/chaterz/java/course/Bombilla.java
@@ -1,27 +1,41 @@
+
 package chaterz.java.course;
 
+
 public class Bombilla {
-	
-	boolean encendido = false;
-	String m_name;
-	
-	public Bombilla(String name) {
-		System.out.println(super.toString() + ": acabo de ser creada");
-		m_name = name;
-	}
-	
-	public void encender() {
-		System.out.println(this + ": me han encendido");
-		encendido = true;
-	}
-	public void apagar() {
-		System.out.println(this + ": me han apagado");
-		encendido = false;
-	}
-	public boolean isEncendido() { return encendido; }
-	
-	@Override
-	public String toString() {
-		return "[" + m_name + "]";
-	}
+
+    boolean encendido = false;
+    String  m_name;
+
+    public Bombilla( String name ) {
+
+        System.out.println( super.toString() + ": acabo de ser creada" );
+        m_name = name;
+    }
+
+    public void encender() {
+
+        System.out.println( this + ": me han encendido" );
+        encendido = true;
+    }
+
+    public void apagar() {
+
+        System.out.println( this + ": me han apagado" );
+        encendido = false;
+    }
+
+    public boolean isEncendido() { return encendido; }
+
+    @Override
+    protected void finalize() {
+
+        System.out.println( this + ": ooohhhhh, me están destruyendo!!!" );
+    }
+
+    @Override
+    public String toString() {
+
+        return "[" + m_name + "]";
+    }
 }
--- HelloWorld/src/chaterz/java/course/HelloWorld.java
+++ HelloWorld/src/chaterz/java/course/HelloWorld.java
@@ -1,23 +1,28 @@
+
 package chaterz.java.course;
 
+
 public class HelloWorld {
-	
-	public static void print_state(Bombilla b) {
-		System.out.println(b + ".isEncendido() == " + b.isEncendido());
-	}
 
-	public static void print_state(Bombilla... bombillas) {
-		for (Bombilla b : bombillas)
-			print_state(b);
-		System.out.println();
-	}
+    public static void print_state( Bombilla b ) {
 
-	public static void main(String[] args) {
-		System.out.println("Buenas!!");
-		Bombilla b1 = new Bombilla("b1");
-		Bombilla b2 = new Bombilla("b2");
-		print_state(b1, b2);
-		b1.encender();
-		print_state(b1, b2);
-	}
+        System.out.println( b + ".isEncendido() == " + b.isEncendido() );
+    }
+
+    public static void print_state( Bombilla... bombillas ) {
+
+        for ( Bombilla b: bombillas )
+            print_state( b );
+        System.out.println();
+    }
+
+    public static void main( String[] args ) {
+
+        System.out.println( "Buenas!!" );
+        Bombilla b1 = new Bombilla( "b1" );
+        Bombilla b2 = new Bombilla( "b2" );
+        print_state( b1, b2 );
+        b1.encender();
+        print_state( b1, b2 );
+    }
 }

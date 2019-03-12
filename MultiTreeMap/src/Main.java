bad33585dfc3997d413073fa82978f78c3da8fac
Luis Colorado <luiscoloradourcola@gmail.com>
2019-03-12 16:34:08 +0200

    TreeMapDemo loop rewritten and using generic type parameters
    Signed-off-by: Luis Colorado <luiscoloradourcola@gmail.com>

--- MultiTreeMap/src/Main.java
+++ MultiTreeMap/src/Main.java
@@ -1,36 +1,40 @@
-import org.jetbrains.annotations.NotNull;
 
 public class Main {
-    public static void main(@NotNull String[] args) {
-        MultilevelTreeMap<String, Integer> map = new MultilevelTreeMap<String, Integer>();
-        for (int i = 0; i < args.length; i++) {
-             //al = "ad";
-           map.put("a",1);
-            map.put("b",2);
-            map.put("c",1);
 
+    public static void main( String[] args ) {
 
-            Integer contador = map.get(args[i]);
-            if (contador == null)
-                contador = new Integer(0);
-            map.put(args[i], contador + 1);
+        MultilevelTreeMap<String, Integer> map
+                = new MultilevelTreeMap<String, Integer>();
+        map.put( "a", 23 );
+        map.put( "b", 2 );
+        map.put( "c", 1 );
+        map.put( "a", 56 );
+        
+        for ( int i = 0; i < args.length; i++ ) {
+
+            Integer contador = map.get( args[i] );
+            if ( contador == null )
+                contador = new Integer( 0 );
+            map.put( args[i], contador + 1 );
         }
-        System.out.println(map);
+        System.out.println( map );
 
-     /*   MultilevelTreeMap<String, Integer> map2 = new MultilevelTreeMap<String, Integer>();
-        for (int a = 0; a< args.length; a++){
-
-           // String a = "paco","alvaro","tomas";
-            Integer contador2 = map2.get(args[a]);
-            if (contador2 == null)
-                contador2 == new Integer(0);
-            map2.put(args[a], contador2 +1);
-
-
-        }
-        System.out.println(map2);
-
-*/
+        /*
+         * MultilevelTreeMap<String, Integer> map2 = new
+         * MultilevelTreeMap<String, Integer>();
+         * for (int a = 0; a< args.length; a++){
+         * 
+         * // String a = "paco","alvaro","tomas";
+         * Integer contador2 = map2.get(args[a]);
+         * if (contador2 == null)
+         * contador2 == new Integer(0);
+         * map2.put(args[a], contador2 +1);
+         * 
+         * 
+         * }
+         * System.out.println(map2);
+         * 
+         */
 
 
     } /* EnD Of tHe Main */
--- MultiTreeMap/src/TreeMapDemo.java
+++ MultiTreeMap/src/TreeMapDemo.java
@@ -1,25 +1,30 @@
 import java.util.*;
+import java.util.Map.Entry;
 class TreeMapDemo {
     public static void main(String args[]) {
 // Create a tree map 
-        TreeMap tm = new TreeMap();
+        TreeMap<String, Double> tm = new TreeMap<String, Double>();
 // Put elements to the map 
-        tm.put("John Doe", new Double(3434.34));
-        tm.put("Tom Smith", new Double(123.22));
-        tm.put("Jane Baker", new Double(1378.00));
-        tm.put("Todd Hall", new Double(99.22));
-        tm.put("Ralph Smith", new Double(-19.08));
-// Get a set of the entries 
-        Set set = tm.entrySet();
-// Get an iterator 
-        Iterator i = set.iterator();
-// Display elements 
-        while(i.hasNext()) {
-            Map.Entry me = (Map.Entry)i.next();
-            System.out.print(me.getKey() + ": ");
-            System.out.println(me.getValue());
+        tm.put("John Doe", 3434.34);
+        tm.put("Tom Smith", 123.22);
+        tm.put("Jane Baker", 1378.00);
+        tm.put("Todd Hall", 99.22);
+        tm.put("Ralph Smith", -19.08);
+//// Get a set of the entries 
+//        Set set = tm.entrySet();
+//// Get an iterator 
+//        Iterator i = set.iterator();
+//// Display elements 
+//        while(i.hasNext()) {
+//            Map.Entry me = (Map.Entry) i.next();
+//            System.out.print(me.getKey() + ": ");
+//            System.out.println(me.getValue());
+//        }
+//        System.out.println();
+        for (Entry<String, Double> entry: tm.entrySet()) {
+            System.out.print(entry.getKey() + ": ");
+            System.out.println(entry.getValue());
         }
-        System.out.println();
 // Deposit 1000 into John Doe's account 
         double balance = ((Double)tm.get("John Doe")).doubleValue();
         tm.put("John Doe", new Double(balance + 1000));

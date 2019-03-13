c0ac848e153815d903928339ee4409bd048e684c
Luis Colorado <luiscoloradourcola@gmail.com>
2019-03-13 17:50:24 +0200

    List of operators and literal numbers/strings

---
+++ ArrayLists/src/curso/java/EratostenesArray.java
@@ -0,0 +1,24 @@
+
+package curso.java;
+
+
+public class EratostenesArray {
+
+    public static final int N = 10000;
+    
+    
+
+    public static void main( String[] args ) {
+
+        boolean[] array = new boolean[N];
+        for ( int i = 2; i < N; i++ ) {
+            if ( array[i] ) {
+                continue;
+            }
+            // no hay marca, luego marcamos todos los multiplos.
+            for (int j = 2*i; j < N; j += i)
+                array[j] = true;
+            System.out.println(i);
+        }
+    }
+}
---
+++ ArrayLists/src/curso/java/EratostenesArrayList.java
@@ -0,0 +1,25 @@
+package curso.java;
+
+import java.util.ArrayList;
+
+public class EratostenesArrayList {
+    
+    public static final int N = 10000;
+
+    public static void main( String[] args ) {
+        ArrayList<Boolean> al = new ArrayList<Boolean>(N);
+        for (int i = 0; i < N; i++)
+            al.add( false );
+        for (int i = 2; i < N; i++) {
+            if (al.get( i ))
+                continue;
+            // no hay marca.
+            System.out.println(i);
+            for (int j = 2*i; j < N; j += i)
+                al.set( j, true );
+        }
+        for (int i = 0; i < N; i++)
+            System.out.println(al.get( i ));
+    }
+
+}

08f51f4dc9a34c6b4045dc8f6a2ecbad6532af85
Luis Colorado <luiscoloradourcola@gmail.com>
2019-03-13 18:07:35 +0200

    Change a method to a constructor

--- Shapes/src/curso/java/Line.java
+++ Shapes/src/curso/java/Line.java
@@ -5,8 +5,8 @@ public class Line extends Shape  {
     public void a() {
         System.out.println("Line.a()");
     }
-    public void Line() {
-        System.out.println("Esto es una Linea.");
+    public Line() {
+        System.out.println("Esto es construir una Linea.");
     }
 
 

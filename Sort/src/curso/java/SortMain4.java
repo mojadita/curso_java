65f24c9c13f4f6f90cd1ae0ea876a8f325ed4875
Luis Colorado <luiscoloradourcola@gmail.com>
2019-03-14 18:20:28 +0200

    Use a lambda
    Signed-off-by: Luis Colorado <luiscoloradourcola@gmail.com>

---
+++ Sort/src/curso/java/SortMain4.java
@@ -0,0 +1,56 @@
+/*
+ * Name: SortMain.java
+ * Author: Luis Colorado <luiscoloradourcola@gmail.com>
+ * Date: 14 mar. 2019 17:28:05
+ * Project: Sort
+ * Package: curso.java
+ * Copyright: (C) 2019 LUIS COLORADO. All rights reserved.
+ */
+
+package curso.java;
+
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+import java.util.Comparator;
+import java.util.TreeSet;
+import java.util.Vector;
+
+
+
+/**
+ * @author lcu
+ *
+ */
+public class SortMain4 {
+
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main( String[] args ) throws Exception {
+
+        int c;
+        String line = "";
+        Vector<String> the_lines = new Vector<String>();
+        while ( (c = in.read()) != -1 ) {
+            switch ( c ) {
+            case '\n':
+            case '\f': /* new line or form feed */
+                the_lines.add( line );
+                line = "";
+                break;
+            default:
+                line += (char) c;
+            } // switch
+        }
+        if ( line.length() > 0 ) {
+            the_lines.add( line );
+        }
+        
+        the_lines.sort( (String a, String b) -> a.compareTo( b ) );
+        for(String l: the_lines)
+            out.println(l);
+    }
+}

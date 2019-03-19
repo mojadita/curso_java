434b9bf568609969e06fc848a68be4741a079157
Luis Colorado <luiscoloradourcola@gmail.com>
2019-03-19 17:17:48 +0200

    NuestraPrimeraCollection

--- Fibonacci/src/curso/java/NewIterator.java
+++ Fibonacci/src/curso/java/NewIterator.java
@@ -2,30 +2,27 @@ package curso.java;
 
 import java.util.Iterator;
 
-public class NewIterator implements Iterable<Long>, Iterator<Long> {
+public abstract class NewIterator implements Iterable<Long>, Iterator<Long> {
+//
+//
+//    private long z = 0;
+//
+//    @Override
+//    public boolean hasNext() {
+//
+//        return z < 1000;
+//    }
+//
+//    @Override
+//    public Long next() {
+//
+//        return ++z;
+//    }
 
-
-    private long z = 0,w = 1;
-
-    @Override
-    public boolean hasNext() {
-
-        return z <= z + 1;
-    }
-
-    @Override
-    public Long next() {
-
-        long resul = z + w;
-        z = w;
-       w = resul;
-        return resul;
-    }
-
-    @Override
-    public Iterator<Long> iterator() {
-
-        return this;
-    }
+//    @Override
+//    public Iterator<Long> iterator() {
+//
+//        return this;
+//    }
 
 }
---
+++ NuestraPrimeraCollection/src/curso/java/NuestraLista.java
@@ -0,0 +1,50 @@
+/* Name: NuestraLista.java
+ * Author: Luis Colorado <luiscoloradourcola@gmail.com>
+ * Date: 19 mar. 2019 15:53:02
+ * Project: NuestraPrimeraCollection
+ * Package: curso.java
+ * Copyright: (C) 2019 LUIS COLORADO.  All rights reserved.
+ */
+package curso.java;
+
+import java.util.Iterator;
+
+/**
+ * @author lcu
+ *
+ */
+public class NuestraLista<E> implements Iterable<E> {
+    final E[] m_losElementos;
+    
+    private class Iter implements Iterator<E> {
+        int m_index = 0;
+        @Override
+        public boolean hasNext() {
+            return m_index < m_losElementos.length;
+        }
+        @Override
+        public E next() {
+            if (!hasNext())
+                throw new ArrayIndexOutOfBoundsException();
+            return m_losElementos[m_index++];
+        }
+    }
+    
+    @SuppressWarnings( "unchecked" )
+    public NuestraLista(int cap) {
+        m_losElementos = (E[]) new Object[cap];
+    }
+    
+    public E get(int i) {
+        return m_losElementos[i];
+    }
+    
+    public void set(int i, E val) {
+        m_losElementos[i] = val;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iter();
+    }
+}

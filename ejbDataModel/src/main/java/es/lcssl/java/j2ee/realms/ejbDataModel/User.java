b2db261dc3ddc1d9ba04391a3ed6546fd8068439
Luis Colorado <luiscoloradourcola@gmail.com>
2019-03-23 14:05:42 +0200

    Initial maven project

---
+++ ejbDataModel/src/main/java/es/lcssl/java/j2ee/realms/ejbDataModel/User.java
@@ -0,0 +1,17 @@
+/*      Name: User.java
+ *    Author: Luis Colorado <luiscoloradourcola@gmail.com>
+ *      Date: 23 mar. 2019 13:56:36
+ *   Project: ejbDataModel
+ *   Package: es.lcssl.java.j2ee.realms.ejbDataModel
+ * Copyright: (C) 2019 Luis Colorado.  All rights reserved.
+ */
+package es.lcssl.java.j2ee.realms.ejbDataModel;
+
+
+/**
+ * @author Luis Colorado {@code <luiscoloradourcola@gmail.com>}
+ *
+ */
+public class User {
+
+}
